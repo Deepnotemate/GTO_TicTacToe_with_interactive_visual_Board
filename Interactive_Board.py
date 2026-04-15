2597e3cf11a7cb6c861cbd8bd9a74357e9aa1f2c
Benjamin Stodd <stoddbenjamin@gmail.com>
2026-04-15 12:38:02 +0200

    added and removed comments from Interactive_Board.py without syntax change

--- Interactive_Board.py
+++ Interactive_Board.py
@@ -1,25 +1,16 @@
-####### we have to still make it possible to also have the ml code begin...
-# also we could implement a random feature in the gto code, so it makes different moves everytime
-# otherwise this runs nice...code could be made prettier though
-
+import numpy as np
+import matplotlib.pyplot as plt
+import time
 
 from GTO_TTT import TicTacToeAI
-import matplotlib.pyplot as plt
-import numpy as np
-import time 
 
-agent = TicTacToeAI()
-###### overall still some improvements to be made... however the grundgerüst steht...
-###### still to do: implement the possibility of having either player start first
-###### also: maybe make the code prettier
-
-# Create the Board ### Not sure yet about the colors of the stripes...
+# Create the Board
 def draw_board(ax, N):
     # Create Overall Board
     board_square = np.zeros((N,N))
     Board = np.kron(0.3*np.ones((3,3)), board_square)
     line = np.ones(3*N)
-    # plotting the lines 
+    # Draw vertical and horizontal grid lines
     ax.plot(N*line, np.linspace(0,3*N,3*N), color = 'gray')
     ax.plot(2*N*line, np.linspace(0,3*N,3*N),color = 'gray')
     ax.plot(np.linspace(0,3*N,3*N), N*line, color = 'gray')
@@ -27,19 +18,17 @@ def draw_board(ax, N):
     return Board
 
 def draw_circle(Board, move,N, color = 0.3):
-    # Create Circles
+    # Draw AI player symbol (circle)
     circle = np.zeros((N,N))
     for i in range(N):
         for j in range(N):
             if ( (i-N/2)**2 + (j-N/2)**2) <= (N/4)**2 and ( (i-N/2)**2 + (j-N/2)**2) >= (N/5)**2:
                 circle[i,j] = color
     Board += np.kron(move.reshape(3,3), circle)
-    #plt.imshow(Board)
-    #plt.show()
     return Board
 
 def draw_cross(Board, move,N, color = 0.3):
-    # Create Cross
+    # Draw human player symbol (X)
     cross = np.zeros((N,N))
     for i in range(N):
         for j in range(N):
@@ -48,31 +37,27 @@ def draw_cross(Board, move,N, color = 0.3):
             if i == N-j and j > 10 and j < N-10:
                 cross[i,j] = color
     Board += np.kron(move.reshape(3,3), cross)
-    #plt.imshow(Board)
-    #plt.show()
     return Board
     
-def convert_board_to_Board(board, ax, N):
-    # takes in a board with notation of 1's and -1's and vizualizes
+def visualize_game_state(board, ax, N):
+    # Convert board state to visual representation
     Board = draw_board(ax, N)
     move = np.zeros(9)
     for i in range(9):
         if board.ravel()[i] == 1:
             tmp_move = move.copy()
             tmp_move[i] = 1
-            # ones are crosses
             Board = draw_cross(Board, tmp_move, N)
         elif board.ravel()[i] == -1:
             tmp_move = move.copy()
             tmp_move[i] = 1
-            # twos are circles
             Board = draw_circle(Board, tmp_move, N)
     #plt.imshow(Board)
+    #plt.show()
     return Board
 
-def draw_move(Board, move, N, cross = False):
-    # Vizualizes the Board + the new move
-    # move has to be a numpy array
+def highlight_move(Board, move, N, cross = False):
+    # Draw move with visual highlight overlay
     if cross:
         Board = draw_cross(Board, move,N , 0.3)
     else:
@@ -80,19 +65,8 @@ def draw_move(Board, move, N, cross = False):
     Board += np.kron(move.reshape(3,3), 0.35*np.ones((N,N)))
     return Board
 
-def un_draw_previous_move(Board, move, N, cross = False):
-    # Vizualizes the Board + the new move
-    # move has to be a numpy array
-    if cross:
-        tmp_Board = draw_cross(Board, move,N , 0.3)
-    else:
-        tmp_Board = draw_circle(Board, move,N , 0.3)
-    Board -= np.kron(move.reshape(3,3), 0.35*np.ones((N,N))) 
-    Board -= tmp_Board
-    return Board
-
-def winning_condition(board):
-    #returns weather board is terminal, as well as the player, who has one
+def check_winner(board):
+    # Check if game is won and return (is_terminal, winner)
     a = np.array([1,1,1])
     b = np.array([-1,-1,-1])
     for i in range(3):
@@ -118,14 +92,9 @@ def on_key(event):
     global current_selection
     global cross
     global placeholder
-    # we need this global function to acess the current_selection variable
-    #
-    # in the following Board references the Matrix used to visualized, while board is the actual tictactoe board
-    #
-    #
     
     if placeholder:
-        win, who = winning_condition(np.sum(boards, axis = 0))
+        win, who = check_winner(np.sum(boards, axis = 0))
         print(f'Player {int(who) +1} wins the game!')
         time.sleep(2)
         print('Thank You for playing!')
@@ -140,9 +109,8 @@ def on_key(event):
         plt.close('all')
     if event.key == 'right':
         Board = np.kron(np.identity(3), np.zeros((N,N)))
-        # we have to type N here explicitly... lets fix having to set it alltogether in the future
         for board in boards:
-            Board += convert_board_to_Board(board, ax, N)
+            Board += visualize_game_state(board, ax, N)
             
         if np.allclose(current_selection, selections[8]):
             idx = -1
@@ -150,41 +118,39 @@ def on_key(event):
             for i in range(8):
                 if np.allclose(current_selection, selections[i]):
                     idx = i
-        current_selection = selections[idx+1]   # resets the data for the move that is being considered
-        Board += draw_move(Board, current_selection,N, cross)
-        marker.set_data(Board) # resets the data for the point that we are plotting
+        current_selection = selections[idx+1]
+        Board += highlight_move(Board, current_selection,N, cross)
+        marker.set_data(Board)
         fig.canvas.draw_idle()
-        # we need this draw_idle() function to make renewed plots
     elif event.key == 'left':
         Board = np.kron(np.identity(3), np.zeros((N,N)))
         for board in boards:
-            Board += convert_board_to_Board(board, ax, N)
+            Board += visualize_game_state(board, ax, N)
     
         for i in range(9):
             if np.allclose(current_selection, selections[i]):
                 idx = i
         current_selection = selections[idx-1]
-        Board += draw_move(Board, current_selection,N, cross)
+        Board += highlight_move(Board, current_selection,N, cross)
         marker.set_data(Board)
         fig.canvas.draw_idle()
         
     elif event.key == 'enter':
         Board = np.kron(0.3*np.ones((3,3)), np.zeros((N,N)))
         for board in boards:
-            Board += convert_board_to_Board(board, ax, N)
-        # check, wheather space is occupied:
+            Board += visualize_game_state(board, ax, N)
         move_index = np.argmax(current_selection.ravel())
-        the_actual_board = np.zeros(9) + np.sum(boards, axis = 0).ravel()
-        if the_actual_board[move_index] != 0:  
+        current_board_state = np.zeros(9) + np.sum(boards, axis = 0).ravel()
+        if current_board_state[move_index] != 0:
             print('That space is already taken!')
 
         else:
-            Board += draw_move(Board, current_selection,N, cross)
+            Board += highlight_move(Board, current_selection,N, cross)
             if cross:
                 boards.append(current_selection )
             else:
                 boards.append(current_selection * -1)
-            win, who = winning_condition(np.sum(boards, axis = 0))
+            win, who = check_winner(np.sum(boards, axis = 0))
             if win:
                 print(f'Player {int(who) +1} wins the game!')
                 time.sleep(2)
@@ -202,12 +168,8 @@ def on_key(event):
             fig.canvas.draw_idle()
             cross = not cross
            
-
-           # # # wir tricksen hier grad noch in dem ersten und zweiten zug, damit es schneller geht
             time.sleep(0.35)
-           
             tmp_board = np.sum(boards, axis=0).ravel()
-          
             if np.count_nonzero(tmp_board == 0) == 9: 
                 move_index = np.random.randint(9)
             elif  np.count_nonzero(tmp_board == 0) == 8:
@@ -215,40 +177,26 @@ def on_key(event):
                     possible_moves = [1,0,1,0,0,0,1,0,1]
                     move_index = np.argsort(possible_moves-np.random.rand(9)/100)[-1]
                 else:
-                    move_index = 4 # this is the only move then :)
-                    #move_index = np.argsort(np.sum(boards, axis = 0).ravel() - np.random.rand(9)/100)[0] # with this there is one way to beat him hehe
-                   
+                    move_index = 4
             else:
                 new_board = agent.gto(tmp_board, cross)[1]
                 move_index = np.argmax(np.abs(new_board-tmp_board.ravel()))
-                print(move_index)
-            # print(move_index)
-         
-          
-            # this was used prior to q_network:
-            #np.argmin(np.sum(boards, axis=0).ravel()-np.random.rand(9)) # selects randomly one of the free spots
           
             current_selection = selections[move_index]
-            #
-            # here we have copied the same code from above... maybe we can make it shorter
-            Board = draw_move(Board, current_selection,N, cross)
+            Board = highlight_move(Board, current_selection,N, cross)
             if cross:
                 boards.append(current_selection)
             else:
                 boards.append(current_selection * -1)
           
-          
             marker.set_data(Board)
             fig.canvas.draw_idle()
             cross = not cross
           
-            win, who = winning_condition(np.sum(boards, axis = 0))
+            win, who = check_winner(np.sum(boards, axis = 0))
             if  who:
                 placeholder = True
 
-
-            print(np.sum(boards, axis=0))
-# #Selections = Possible Moves in one array
 selections = np.zeros((9,3,3))
 for i in range(9):
     selections[i].ravel()[i] = 1
@@ -262,18 +210,19 @@ N = 100
 
 placeholder = False
 cross = False
-alert = False
 boards = []
+agent = TicTacToeAI()
 draw_board(ax, N)
 
 first_move = np.array([0,0,0,0,0,0,0,0,0])
 first_move[np.random.randint(9)] = 1 
 boards.append(first_move.reshape(3,3))
 
-Board = convert_board_to_Board(current_selection, ax, N)
-move = draw_move(Board, current_selection,N, cross = False)
-marker = ax.imshow(move, cmap = 'gist_earth')
 
+Board = visualize_game_state(current_selection, ax, N)
+
+move = highlight_move(Board, current_selection,N, cross = False)
+marker = ax.imshow(move, cmap = 'gist_earth')
 
 fig.canvas.mpl_connect('key_press_event', on_key)
 plt.show()
